db5b0fbe6c231298c38918bb8d70954be248f57a
BESoft33 <superstargajanikant@gmail.com>
2024-01-21 16:26:51 +0545

    redesigned admin interface

--- crowcrows/crowapp/admin.py
+++ crowcrows/crowapp/admin.py
@@ -1,15 +1,20 @@
 from django.contrib import admin
 from .models import *
+
+
 # Register your models here.
 # @admin.register(Blogger)
 class BloggerAdmin(admin.ModelAdmin):
-    # model=Blogger
-    list_display=('first_name', 'last_name')
+    list_display = ('first_name', 'last_name', 'role',)
+
 
 # @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    list_display=('blogger','title','published_on',)
-    model=Article
+    list_display = ('created_by', 'title', 'published_on',)
+    model = Article
+
 
 admin.site.register(Article, ArticleAdmin)
-admin.site.register(Blogger,BloggerAdmin)
+admin.site.register(Author, BloggerAdmin)
+admin.site.register(Subscriber, BloggerAdmin)
+admin.site.register(Editor, BloggerAdmin)
