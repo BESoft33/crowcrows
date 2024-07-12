d2011bee522d0c1be12c2c4140820f979e0f7aae
BESoft33 <superstargajanikant@gmail.com>
2024-07-12 18:57:20 +0545

    migrated django-based views to restframework based views

---
+++ crowcrows/authentication/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from crowapp.models import User
+from django.core.exceptions import ValidationError
+
+class SignupSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    password_confirm = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'password', 'password_confirm']
+
+    def validate(self, data):
+        if data['password'] != data['password_confirm']:
+            print('passwords match')
+            raise ValidationError("Passwords do not match.")
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop('password_confirm')
+        user = User.objects.create_user(
+            first_name=validated_data.get('first_name'),
+            last_name=validated_data.get('last_name'),
+            email=validated_data.get('email'),
+            password=validated_data.get('password')
+        )
+        return user
